72bababff3b032867ae23bf2fc432c776f50cd06
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-06-25 12:12:49 -0300

    Gallery comp created and Home updated

---
+++ src/components/Gallery.svelte
@@ -0,0 +1,54 @@
+<script>
+  import { onMount } from 'svelte'
+
+  // State
+  let images = []
+
+  let imagesRef = firebase.storage().ref().child('images/images/')
+
+  async function listImages () {
+    try {
+      let res = await imagesRef.listAll()
+      res.items.forEach( async itemRef => {
+        let imgURL = await itemRef.getDownloadURL()
+        images = [...images, imgURL]
+      });
+      images = images
+    } catch (error) {
+      console.log(error)
+      alert('Ups, something happened, come and see the console!')
+    }
+  }
+
+  onMount(async () => {
+    await listImages()
+    console.log('finished', images)
+  })
+</script>
+
+{#each images as img}
+  <figure>
+    <img src={img} alt="something nice and pretty">
+    <figcaption>my personal collection image</figcaption>
+  </figure>
+{:else}
+  <p>Loading _>:P)...</p>
+{/each}
+<!-- this block renders when photos.length === 0 -->
+
+<style>
+
+  img {
+    max-width: 100%;
+    padding: 2em 0;
+  }
+  figure {
+    text-align: center;
+  }
+  figcaption {
+    padding: 2em 0;
+  }
+
+
+
+</style>
---
+++ src/components/Home.svelte
@@ -0,0 +1,72 @@
+
+<script>
+    import saveImage from '../services/saveImage.js'
+  
+    // State
+    export let name
+    export let setShowGallery
+
+  
+    let storage = firebase.storage()
+    let imageStorage = storage.ref('images/')
+      
+    let currentImage
+  
+  
+    function fileCLick() {
+      document.querySelectorAll('#file')[0].click()
+    }
+  
+    async function changeImage(e) {
+      const file = e.target.files[0]
+  
+      currentImage = window.URL.createObjectURL(e.target.files[0])
+      console.log(currentImage)
+      await saveImage(imageStorage, file)
+    }
+  
+
+  // lordvlaim
+  </script>
+
+<main>
+	<h1>Hello {name}!</h1>
+  <button on:click={setShowGallery}>
+    All images
+  </button>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+  <input 
+    id="file" 
+    type="file" 
+    accept="image/*" 
+    style="display: none;"
+    on:change={changeImage}
+  />
+  
+  <button class="primary" on:click={fileCLick}>Upload <strong>IMG</strong></button>
+  <img src={currentImage ? currentImage : "#"} alt="Something maybe">
+  <!-- Validate the files in the server.  -->
+</main>
+
+<style>
+
+    main {
+    display: grid;
+    place-items: center;
+    min-height: 100vh;
+
+	}
+
+  img {
+    max-width: 100%;
+  }
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+</style>

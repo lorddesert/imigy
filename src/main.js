ee6290f3f30333763d459fb915fa459f0d7871ae
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-06-25 12:13:19 -0300

    main js updated

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import App from './components/App.svelte';
+import App from './components/App.svelte'
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCOP9rpHYSA465DReJ0AnhFQTXVpqFYSQI",
+    authDomain: "imigy-5baee.firebaseapp.com",
+    projectId: "imigy-5baee",
+    storageBucket: "imigy-5baee.appspot.com",
+    messagingSenderId: "110918614375",
+    appId: "1:110918614375:web:f4f35b5fc7955d425a83c2",
+    measurementId: "G-6QFR6WL4G3"
+  };
+  // Initialize Firebase
+  let fapp = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+  // firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 const app = new App({
 	target: document.body,

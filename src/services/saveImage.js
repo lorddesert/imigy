b35e31abee1b4289bc96d7dab7d892f923860d1c
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-09-22 09:18:18 -0300

    change the error message for one better

--- src/services/saveImage.js
+++ src/services/saveImage.js
@@ -7,6 +7,6 @@ export default async function saveImage(ref, file, uid) {
   } catch (error) {
       console.log(error)
       alert(`Ups, something happened: ${error}\n
-      In case that this error appears multiple times, send a snapshot of this error to the dev.`)
+      In case that this error appears multiple times, send a snapshot of this error to the support.`)
   }
 }

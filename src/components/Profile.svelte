d583194a17dfd95489b28c857b54d7793db2927b
Sacha Nocetto <sacha.nocetto@hotmail.com>
2022-01-14 12:13:17 -0300

    create Profile component and its logic

---
+++ src/components/Profile.svelte
@@ -0,0 +1,57 @@
+<script>
+</script>
+
+<header>
+    <div id="profile-image">
+        <img src="/favicon.png" alt="profile" />
+    </div>
+    <form on:submit={() => alert('asdasd')}>
+        <input class="input is-rounded" type="search" name="search" id="search" placeholder="See for any profile!" on/>
+        <button class="button is-danger is-outlined is-rounded control">Search</button>
+    </form>
+    <!-- <div>Options</div> -->
+</header>
+<main />
+
+<style>
+    main {
+        display: grid;
+        place-items: center;
+        /* min-height: 100vh; */
+        font-family: "Dosis", sans-serif;
+    }
+
+    header {
+        display: grid;
+        place-content: center;
+        place-items: center;
+        padding-top: 5em;
+        gap: .5em;
+        grid-template-columns: 50px 350px;
+        transition: all ease .35s;
+    }
+    #profile-image {
+        display: grid;
+        place-items: center;
+        padding: .3em;
+        background-color: var(--font-color);
+        border-radius: 50%;
+    }
+    #profile-image:hover {
+        cursor: pointer;
+    }
+    img {
+        max-width: 100%;
+    }
+    form {
+        display: flex;
+        gap: .5em;
+        width: 100%;
+    }
+
+    @media (min-width: 600px) {
+        header {
+            padding-top: 5vh;
+        }
+    }
+</style>
